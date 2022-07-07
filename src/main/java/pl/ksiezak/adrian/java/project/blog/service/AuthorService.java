b663cfeb47870ee84a080cece1645efd3a36198c
Adrian Księżak <adrianks@op.pl>
2022-07-07 11:13:21 +0200

    Change from System.out.println into Logger for Service.

--- src/main/java/pl/ksiezak/adrian/java/project/blog/service/AuthorService.java
+++ src/main/java/pl/ksiezak/adrian/java/project/blog/service/AuthorService.java
@@ -1,31 +1,35 @@
 package pl.ksiezak.adrian.java.project.blog.service;
 
-import pl.ksiezak.adrian.java.project.blog.web.model.Author;
 import pl.ksiezak.adrian.java.project.blog.repository.AuthorDao;
+import pl.ksiezak.adrian.java.project.blog.web.model.Author;
+
+import java.util.logging.Logger;
 
 public class AuthorService {
+    private static final Logger LOGGER = Logger.getLogger(AuthorService.class.getName());
+
     private AuthorDao authorDao = new AuthorDao();
 
     public Author create(Author author){
-        System.out.println("create(" + author + ")");
+        LOGGER.info("create(" + author + ")");
         Author authorFromAuthorDao = authorDao.create(author);
         return authorFromAuthorDao;
     }
 
     public Author read(int id){
-        System.out.println("read(" + id + ")");
+        LOGGER.info("read(" + id + ")");
         Author readAuthor = authorDao.read(id);
         return readAuthor;
     }
 
     public Author update(int id, String nick){
-        System.out.println("update(" + id + nick + ")");
+        LOGGER.info("update(" + id + ", " + nick + ")");
         Author updateAuthor = authorDao.update(id, nick);
         return updateAuthor;
     }
 
     public void delete(int id){
-        System.out.println("delete(" + id + ")");
+        LOGGER.info("delete(" + id + ")");
         authorDao.delete(id);
     }
 }
